d541846fd385967d5e86e7ada3240e920da1fe38
daenom <sahilmoon944@gmail.com>
2026-06-26 14:10:35 +0530

    feat(backend): initialize spring boot backend

---
+++ backend/src/test/java/com/daenom/workout/WorkoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daenom.workout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
